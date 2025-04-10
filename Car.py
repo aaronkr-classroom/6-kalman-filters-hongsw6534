95b7f317744f2511f63991254a12134f2255701b
hongsw6534 <hongsw1006@gmail.com>
2025-04-10 18:41:13 +0900

    finish

--- Car.py
+++ Car.py
@@ -16,7 +16,7 @@ class Car(object):
     
     # Car constructor 
     # Called when you write car.Car(_, _, _)
-    def __init__(self, position, velocity, world):
+    def __init__(self, position, velocity, world, color = 'r'):
         """Initializes Car with some position, velocity, and a world to traverse."""
         
         # Initialize the state
@@ -25,9 +25,20 @@ class Car(object):
         self.world = world # world is a 2D list of values that range from 0-1
         
         # Set the default color
-        self.color = 'r'
+        self.color = color
         
         # Initalize the path
+        self.path = []
+        self.path.append(position)
+
+    # Reset initial position and velocity
+    def reset_pos(self, position = [0,0]):
+        """Resets the car's position and velocity to the given values."""
+        
+        # Reset the state
+        self.state[0] = position
+        
+        # Reset the path to only include the new position
         self.path = []
         self.path.append(position)
         
